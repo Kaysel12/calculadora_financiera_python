e732fcd42604dff1dc2ff1b07e77bb07de8138df
kaysel nunez <kaysel@codika.net>
2024-09-04 15:40:16 -0400

    subida de cambios

--- inversiones/settings.py
+++ inversiones/settings.py
@@ -83,18 +83,18 @@ WSGI_APPLICATION = 'inversiones.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'inversiones',
-        'USER': 'root',
-        'PASSWORD': '123456789',
-        'HOST': 'host.docker.internal',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'inversiones',
+    #     'USER': 'root',
+    #     'PASSWORD': '123456789',
+    #     'HOST': 'host.docker.internal',
+    #     'PORT': '3306',
+    # }
 }
 
 
@@ -168,3 +168,5 @@ SPECTACULAR_SETTINGS = {
     #   license=openapi.License(name="BSD License"),
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS
